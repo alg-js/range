e5a7f4ab7a99a3578820f1219150e725711d4be7
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2025-05-11 23:06:43 +1200

    clean up default exports

---
+++ src/types.d.ts
@@ -0,0 +1,53 @@
+import type { Range } from "./main.js";
+
+/**
+ * An interface for hashable objects
+ */
+export interface Hashable<T> {
+  hash(): number;
+  equals(other: T): boolean;
+}
+
+/** A comparator function */
+export type Comparator = (a: number, b: number) => number;
+
+/** A predicate function */
+export type Predicate = (value: number, index: number, range: Range) => unknown;
+
+/** A predicate function with `this` bound to a value */
+export type BoundPredicate<T> = (
+  this: T,
+  value: number,
+  index: number,
+  range: Range,
+) => unknown;
+
+/** A mapping function */
+export type Mapping<V> = (value: number, index: number, range: Range) => V;
+
+/** A mapping function with `this` bound to a value */
+export type BoundMapping<T, V> = (
+  this: T,
+  value: number,
+  index: number,
+  range: Range,
+) => V;
+
+/** A consumer function */
+export type Consumer = (value: number, index: number, range: Range) => void;
+
+/** A consumer function with `this` bound to a value */
+export type BoundConsumer<T> = (
+  this: T,
+  value: number,
+  index: number,
+  range: Range,
+) => void;
+
+/** A reducer/folding function */
+export type Reducer<T> = (
+  accumulator: T,
+  value: number,
+  index: number,
+  range: Range,
+) => void;
